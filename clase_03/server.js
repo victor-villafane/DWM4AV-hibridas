4e1f135185ab9ff1ec3f797a4bb671428dad6c92
victor-villafane <victor.villafane@davinci.edu.ar>
2025-08-22 13:02:03 -0300

    Clase-03

---
+++ clase_03/server.js
@@ -0,0 +1,50 @@
+// const http = require("http") //importamos usando commonjs
+// //import http from "http" -> necesitamos mas configuracion
+// const listado = require("./data/productos.js")
+// const pages = require("./pages/utils.js")
+
+import http from "http"
+import listado from "./data/productos.js"
+import { createProductList, createPage } from "./pages/utils.js"
+import { readFile } from "fs"
+
+const server = http.createServer( function(request, response){
+    console.log(request.url)
+
+    switch(request.url){
+        case "/":
+            response.write( createPage("Home", "Juan Perez") )
+            break
+        case "/materia":
+            response.write( createPage("Materia", "Aplicaciones Hibridas") )
+            break
+        case "/profesor":
+            response.write( createPage("Profesor", "Pepe perez") )
+            break            
+        case "/productos":
+            response.write( createPage("Listado de productos", createProductList(listado)) )
+            break
+        case "/cosa.html":
+            readFile('./public/cosa.html',"utf-8", (err, data) => {
+                if (err) throw err;
+                response.write(data)
+                response.end()
+              });     
+            break
+        case "/style.css":
+            readFile('./public/style.css',"utf-8", (err, data) => {
+                if (err) throw err;
+                    response.write(data)
+                    response.end()
+                });     
+            break            
+        default:
+            response.write( createPage("Materia", "No se encontro") )
+            break
+    }
+    // response.end()
+} )
+
+server.listen( 2023, () => {
+    console.log("Todo funcionando en en puerto 2023")
+})
