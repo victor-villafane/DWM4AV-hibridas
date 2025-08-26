c241b8a6acc7ae11fb04c637a168eb13bd14e9dc
victor-villafane <166618280+victor-villafane@users.noreply.github.com>
2025-08-26 10:11:28 -0300

    Clase-04 sin express

---
+++ clase_04/data/productos.js
@@ -0,0 +1,20 @@
+const listado = [
+    {
+        id: 1,
+        nombre: "Café Expreso",
+        precio: 123
+    },
+    {
+        id: 2,
+        nombre: "Cafe Latte",
+        precio: 200
+    },
+    {
+        id: 3,
+        nombre: "Cafe Americano",
+        precio: 150
+    }
+]
+// module.exports = listado commonjs
+export default listado
+// export { listado }
---
+++ clase_04/readme.md
@@ -0,0 +1,11 @@
+## Proyecto: Sitio Web Estático
+
+Crea un sitio web estático sobre el tema que prefieras.
+
+- Debe tener **dos páginas**:
+    - `home.html` (página principal)
+    - `contacto.html` (página de contacto)
+
+- Utiliza estilos en un archivo `.css` separado.
+
+- Incluye imágenes de cualquier tipo.
