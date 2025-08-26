50a43b8963236763e88150729290dec301233d3a
victor-villafane <166618280+victor-villafane@users.noreply.github.com>
2025-08-26 10:51:53 -0300

    clase-04 con express

--- clase_04/server.js
+++ clase_04/server.js
@@ -1,64 +1,20 @@
-// const http = require("http") //importamos usando commonjs
-// //import http from "http" -> necesitamos mas configuracion
-// const listado = require("./data/productos.js")
-// const pages = require("./pages/utils.js")
-
-import http from "http"
-import listado from "./data/productos.js"
-import { createProductList, createPage } from "./pages/utils.js"
+import express from "express"
 import { readFile } from "fs"
 
-const server = http.createServer( function(request, response){
-    console.log(request.url)
+const app = express()
 
-    switch(request.url){
-        case "/":
-            response.write( createPage("Home", "Juan Perez") )
-            break
-        case "/materia":
-            response.write( createPage("Materia", "Aplicaciones Hibridas") )
-            break
-        case "/profesor":
-            response.write( createPage("Profesor", "Pepe perez") )
-            break            
-        case "/productos":
-            response.write( createPage("Listado de productos", createProductList(listado)) )
-            break
-        case "/home":
-            readFile('./public/home.html',"utf-8", (err, data) => {
-                if (err) throw err;
-                response.write(data)
-                response.end()
-            });     
-            break
-        case "/contacto":
-            readFile('./public/contacto.html',"utf-8", (err, data) => {
-                if (err) throw err;
-                response.write(data)
-                response.end()
-            });     
-            break            
-        case "/style.css":
-            readFile('./public/style.css',"utf-8", (err, data) => {
-                if (err) throw err;
-                    response.write(data)
-                    response.end()
-                });     
-            break    
-        case "/img/1.png":
-            readFile('./public/img/1.png', (err, data) => {
-                if (err) throw err;
-                    response.write(data)
-                    response.end()
-                });     
-            break                   
-        default:
-            response.write( createPage("Materia", "No se encontro") )
-            break
-    }
-    // response.end()
-} )
+app.use( "/", express.static("public") )
 
-server.listen( 2023, () => {
-    console.log("Todo funcionando en en puerto 2023")
+app.get("/home", (req, res) => {
+    console.log(req.query)
+    const nombre = req.query.nombre
+    const apellido = req.query.apellido
+    res.send("Hola " + nombre + " " + apellido)
 })
+
+app.post("/home", (req, res) => {
+    console.log(req.body)
+    res.send("Esto es un post")
+})
+
+app.listen(2025, () => console.log("Funcionando"))
