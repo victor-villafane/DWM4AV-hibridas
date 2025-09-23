7e1d36e55d6268dbbc8fac11087e4f9e6469b22e
victor-villafane <166618280+victor-villafane@users.noreply.github.com>
2025-09-23 10:34:52 -0300

    documentacion con swagger

--- clase_11/index.js
+++ clase_11/index.js
@@ -2,16 +2,20 @@ import express from "express"
 import ProductosRoute from "./routes/productos.route.js"
 import ProductosApiRoute from "./api/routes/productos.api.routes.js"
 import ClientesApiRoute from "./api/routes/clientes.api.routes.js"
+import swaggerFile from './swagger.output.json' with { type: 'json' }
+import swaggerUI from 'swagger-ui-express'
+
 const app = express()
 
 app.use( express.urlencoded({extended: true}) )
 app.use( express.json() )
 //app.use( express.static("/public") )
+app.use( '/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile) )
 
 app.use( "/productos",ProductosRoute )
 app.use( "/api/productos",ProductosApiRoute )
 app.use("/api/clientes", ClientesApiRoute)
 
-app.listen(2025, () => {
+app.listen(3000, () => {
     console.log("funcionando")
 })
